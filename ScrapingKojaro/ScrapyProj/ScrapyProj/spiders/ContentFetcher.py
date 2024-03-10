a49565085521ddf6bfb316ecba2a0552b6a0eeec
HoseinNasiriShahraki <hosein.nasiri@outlook.com>
2024-03-10 10:51:44 +0330

    Push rep

--- ScrapingKojaro/ScrapyProj/ScrapyProj/spiders/ContentFetcher.py
+++ ScrapingKojaro/ScrapyProj/ScrapyProj/spiders/ContentFetcher.py
@@ -7,9 +7,21 @@ class ContentfetcherSpider(scrapy.Spider):
     allowed_domains = ["www.kojaro.com"]
     start_urls = url_list[0]
     
+    def start_requests(self): 
+        url_list = get_link()     # Enter your target URL here 
+        for url in url_list: 
+            yield scrapy.Request(url=url, callback=self.parse)
+    
+    
     def parse(self, response):
         yield {
-            'URL': response.xpath('.//a/@href').get(),
-            'Quote': response.xpath('.//p/text()').get(),
+            'Name': response.xpath('.//*[@id="AttractionSearch"]/div[1]/div/div/div[1]/div[1]/h1/span/text()').get(),
+            'Address': response.xpath('.//*[contains(concat( " ", @class, " " ), concat( " ", "address", " " ))]/text()').get(),
+            'Phone Number': response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "phoneNumberDirecttion", " " ))]/text()').get(),
+            'Email': response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "dis-inline", " " )) and contains(concat( " ", @class, " " ), concat( " ", "medium", " " ))]/text()').get(),
+            'Web Site': response.xpath('.//*[contains(concat( " ", @class, " " ), concat( " ", "text-darken-3", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "font-rem", " " ))]/text()').get(),
+            'Rating': response.xpath('.//*[contains(concat( " ", @class, " " ), concat( " ", "showRateHotel", " " ))]/text()').get(),
+            'Country': response.xpath('.//*[contains(concat( " ", @class, " " ), concat( " ", "attraction-heritage", " " ))]//span/text').get(),
+            'Body': response.xpath('//sub | //*[(@id = "bodyContainer")]//p/text()').extract() #this returns a list of docs
         }
         
